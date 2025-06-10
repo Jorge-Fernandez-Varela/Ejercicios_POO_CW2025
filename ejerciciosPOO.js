ae355fdef441773c8a785eaf5d9a562c4ad5cd07
Erickdced <32326086@alumnp.enp.unam.mx>
2025-06-09 20:36:35 -0500

    guardado de clase animal e instanciación de majolote

--- ejerciciosPOO.js
+++ ejerciciosPOO.js
@@ -1,1 +1,33 @@
 //Este archivo es para que puedan realizar los ejercicios sobre POO
+
+class Animal
+{
+    constructor(weight, numeroPatas, color, altura, posicion)
+    {
+        this.peso = weight;
+        this.numeroPatas = numeroPatas;
+        this.color = color;
+        this.altura = altura;
+        this.estaVivo = true;
+        this.posicion = posicion;
+    }
+    avanzar()
+    {
+        this.posicion += 1;
+        console.log(this.posicion);
+    }
+    crecer()
+    {
+        this.altura += 10;
+        this.peso += 2;
+
+    }
+    comer(comida)
+    {
+        console.log("estoy comiendo " + comida);
+        //console.log(`estoy comiendo ${comida}`);
+        this.peso += 4;
+    }
+}
+
+let majolote = new Animal(50, 4, "Rosa", 15, 1);
