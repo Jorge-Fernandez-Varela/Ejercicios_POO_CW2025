1f44107a97bc090d21ff386354168781effba62f
Alexei140602 <133258992+Alexei140602@users.noreply.github.com>
2025-06-10 00:06:12 -0600

    add calculadora.js

---
+++ Dynamics/JS/calculadora.js
@@ -0,0 +1,30 @@
+
+
+class Calculadora{
+
+    suma(var1,var2)
+    {
+        
+    }  
+
+    resta(var1,var2)
+    {
+        
+
+    }
+
+    mulplicacion(var1,var2)
+    {
+       
+
+    }
+
+    division(var1,var2)
+    {
+    
+    }
+}
+
+
+const var1 =2;
+const var2=10;
